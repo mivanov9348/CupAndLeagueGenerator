0dd060cc9f0899410d7d945b80fdd54398c3ecbe
mivanov93 <mivanov93@students.softuni.bg>
2022-05-22 21:06:35 +0300

    EditLeagueSettings

--- CupLeagueGenerator.Infrastructure/Models/LeagueModel.cs
+++ CupLeagueGenerator.Infrastructure/Models/LeagueModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace CupLeagueGenerator.Infrastructure.Models
 {
+    using CupLeagueGenerator.Infrastructure.Data.DataModels;
     public class LeagueModel
     {
         public string LeagueName { get; set; }
         public int NumberOfTeams { get; set; }
         public int TeamsPerGroup { get; set; }
+        public List<string> Teams { get; set; }
+
+        public List<Fixture> Fixtures { get; set; }
     }
 }
